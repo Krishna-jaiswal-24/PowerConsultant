4a0fdb6117f851f4d775455ee94c184c70efa278
unknown <jainadi1717@gmail.com>
2024-05-29 16:31:06 +0530

    minor change

--- frontend/src/components/AdminNavbar.jsx
+++ frontend/src/components/AdminNavbar.jsx
@@ -37,7 +37,7 @@ function AdminNavbar({ UserName }) {
             </li>
             <li className='flex sm:items-center'>
               <img src="https://www.pikpng.com/pngl/m/16-169348_user-icon-user-white-icon-transparent-clipart.png" className="h-8  mx-3 rounded-md" alt="User Icon" />
-              <a className="block mt-1 py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{UserName}</a>
+              <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{UserName}</a>
             </li>
           </ul>
         </div>
